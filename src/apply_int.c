ae5f8814d91fc32519284e498c74f125d256b957
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-10 16:46:13 -0300

    refactor: apply_int

--- src/apply_int.c
+++ src/apply_int.c
@@ -85,28 +85,27 @@ static char	*get_pad_p(int size)
 int	apply_precision_int(char **str, t_arg *arg)
 {
 	char	*res;
-	char	*nbr;
 	char	*temp;
 	uint8_t	is_neg;
 
 	if (arg->precision < 0)
 		return (0);
 	is_neg = **str == '-';
-	nbr = ft_substr(*str, is_neg, ft_strlen(*str));
 	temp = get_pad_p(arg->precision - ft_strlen(*str) + is_neg);
+	if (arg->precision == 0 && ft_strncmp(*str, "0", ft_strlen(*str)) == 0)
+		res = ft_strjoin(temp, "");
+	else
+		res = ft_strjoin(temp, (*str) + is_neg);
+	safe_free((void **)&temp);
 	safe_free((void **)str);
 	if (is_neg)
 	{
-		*str = ft_strjoin("-", temp);
-		res = ft_strjoin(*str, nbr);
-		safe_free((void **)str);
+		temp = ft_strjoin("-", res);
+		safe_free((void **)&res);
+		res = temp;
 	}
-	else if (arg->precision == 0 && ft_strncmp("0", nbr, ft_strlen(nbr)) == 0)
-		res = ft_strdup("");
-	else
-		res = ft_strjoin(temp, nbr);
-	safe_free((void **)&temp);
-	safe_free((void **)&nbr);
 	*str = res;
-	return (*str == NULL);
+	if (*str == NULL)
+		return (FT_PRINTF_ERROR);
+	return (0);
 }
